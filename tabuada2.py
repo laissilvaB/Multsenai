e65a1d3d8013e1a8f92c6af6079d2b1b50d90255
laissilvaB <151771918+laissilvaB@users.noreply.github.com>
2023-11-27 16:00:45 -0300

    adicionando tabuada2.py

---
+++ tabuada2.py
@@ -0,0 +1,21 @@
+# tabuada
+
+# apresentação
+
+print('\n\t\t\t -- multiplicação--\n')
+
+# entrada
+
+n = int(input('qual o numero da tabuada'))
+
+print(n, "x 0 =", n * 0)
+print(n, "x 0 =", n * 1)
+print(n, "x 0 =", n * 2)
+print(n, "x 0 =", n * 3)
+print(n, "x 0 =", n * 4)
+print(n, "x 0 =", n * 5)
+print(n, "x 0 =", n * 6)
+print(n, "x 0 =", n * 7)
+print(n, "x 0 =", n * 8)
+print(n, "x 0 =", n * 9)
+print(n, "x 0 =", n * 10)
